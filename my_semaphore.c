37fb5723247b51a85458874f12134c37298e2343
pedronaresi <pmnaresi@hotmail.com>
2018-06-17 10:28:08 -0300

    Busca em profundidade add

--- my_semaphore.c
+++ my_semaphore.c
@@ -45,6 +45,21 @@ typedef struct{
     int NArestas;
 }TGrafo;
 
+typedef struct Celula{
+    int item;
+    struct TCelulaPilha *proximo;
+}TCelulaPilha;
+
+typedef struct Pilha{
+    TCelulaPilha *topo;
+}TPilha;
+
+//Global variables
+long vertices[MaxNumVertices];
+int quantidade_de_vertices = 0;
+TGrafo grafo;
+int aux = 0;
+
 TLista *tlista_inicia(){
     TLista *pLista = (TLista*)malloc(sizeof(TLista));
     pLista->primeiro=NULL;
@@ -94,36 +109,101 @@ int tgrafo_inserearesta(TGrafo *pgrafo, int u, int v){
     pgrafo->NArestas++;
     return 0;
 }
-/*
-// do a depth first search
-void do_dfs(Vertex *vertex, int* count) {
-    Node* p = vertex->list;
-    vertex->mark = ++(*count);
-    visit (vertex);
-    while (p != 0) {
-        if (!p->vertex->mark) {
-            do_dfs (p->vertex, count);
-        }
-        p = p->next;
-    }
-}
-void dfs(Vertex *graph[]) {
-    int i;
-    int count = 0;
-    // set all to unvisited
-    for (i = 0; i < NUM_VERTEX; i ++) {
-        graph[i]->mark = 0;
-    }
-    // each vertex dfs it
-    for (i = 0; i < NUM_VERTEX; i ++) {
-        if (graph[i]->mark == 0) {
-            do_dfs (graph[i], &count);
-        }
-    }
-}*/
 
-long vertices[MaxNumVertices];
-int quantidade_de_vertices = 0;
+TPilha* criaPilha(void){
+    TPilha *p = (TPilha*)malloc(sizeof(TPilha));
+    p->topo=NULL;
+    return p;
+}
+
+int PilhaEhVazia(TPilha* p){
+    if(p->topo==NULL)
+        return 1;
+    else
+        return 0;
+}
+
+void Empilha(TPilha* p, int v){
+    TCelulaPilha *novo = (TCelulaPilha*)malloc(sizeof(TCelulaPilha));
+    novo->item=v;
+    novo->proximo=p->topo;
+    p->topo=novo;
+}
+
+float Desimpilha(TPilha* p){
+    float v;
+    TCelulaPilha* aux;
+    if(PilhaEhVazia(p)==1){
+        exit(1);
+    }else{
+        aux=p->topo;
+        v=aux->item;
+        p->topo=aux->proximo;
+        free(aux);
+        return v;
+    }
+
+}
+
+bool dfs(int v)
+{
+	TPilha *pilha = criaPilha();
+	bool visitados[grafo.NVertices], pilha_rec[grafo.NVertices];
+
+	// inicializa visitados e pilha_rec com false
+	for(int i = 0; i < grafo.NVertices; i++)
+		visitados[i] = pilha_rec[i] = false;
+
+	// faz uma DFS
+	while(true)
+	{
+		bool achou_vizinho = false;
+		TCelula *aux;
+
+		if(!visitados[v]) {
+			Empilha(pilha, v);
+			visitados[v] = pilha_rec[v] = true;
+		}
+
+		aux = grafo.Adj[v]->primeiro;
+		while(aux != grafo.Adj[v]->ultimo)
+		{
+			// se o vizinho já está na pilha é porque existe ciclo
+			if(pilha_rec[aux->vert])
+				return true;
+			else if(!visitados[aux->vert])
+			{
+				// se não está na pilha e não foi visitado, indica que achou
+				achou_vizinho = true;
+				break;
+			}
+			aux = aux->proximo;
+		}
+
+		if(!achou_vizinho)
+		{
+			pilha_rec[pilha->topo->item] = false; // marca que saiu da pilha
+			Desimpilha(pilha); // remove da pilha
+			if(PilhaEhVazia(pilha))
+				break;
+			v = pilha->topo->item;
+		}
+		else
+			v = grafo.Adj[v]->ultimo->vert;
+	}
+
+	return false;
+}
+
+bool temCiclo()
+{
+	for(int i = 0; i < grafo.NVertices; i++)
+	{
+		if(dfs(i))
+			return true;
+	}
+	return false;
+}
 
 int converteVertice(long valor_do_vertice){
     int i;
@@ -138,8 +218,6 @@ int converteVertice(long valor_do_vertice){
     return i;
 }
 
-TGrafo grafo;
-int aux = 0;
 
 int (*_sem_wait)(sem_t *) = NULL;
 
@@ -149,13 +227,18 @@ int sem_wait(sem_t *sem) {
 		_sem_wait = dlsym(RTLD_NEXT, "sem_wait");
 		/* Irá apontar para o sem_wait original*/
 	}
-	if (aux == 0){
-		aux = 1;
-		tgrafo_inicia(&grafo, MaxNumVertices);
+	if (temCiclo() == FALSE) {
+		if (aux == 0){
+			aux = 1;
+			tgrafo_inicia(&grafo, MaxNumVertices);
+		}
+		tgrafo_inserearesta(&grafo, converteVertice((size_t)sem), converteVertice(pthread_self()));
+		printf("\t Semaforo %ld na thread %ld\n", (size_t)sem, pthread_self());
+		printf("\t Dentro da sem_wait()... faça o que quiser aqui!\n");
+		r = _sem_wait(sem);
+		return(r);
+	}else{
+		printf("\t Encontrou deadlock\n");
+		return 0;
 	}
-	tgrafo_inserearesta(&grafo, converteVertice((size_t)sem), converteVertice(pthread_self()));
-	printf("\t Semaforo %ld na thread %ld\n", (size_t)sem, pthread_self());
-	printf("\t Dentro da sem_wait()... faça o que quiser aqui!\n");
-	r = _sem_wait(sem);
-	return(r);
 }
